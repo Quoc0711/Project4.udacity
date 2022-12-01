45e117f09dcbb94495237913dc3ec6edc02292f8
Quoc0711 <Quoctc11@gmail.com>
2022-12-01 15:08:12 +0700

    updated code

--- c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -4,11 +4,22 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { createTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
 
+const logger = createLogger('createTodo');
 // TODO: create TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Handling createTodo event', {event});
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    if (!newTodo.name) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'Error: Name of to do task is empty !'
+        })
+      }
+    }
     const todo = await createTodo(event, newTodo);
 
     return {
