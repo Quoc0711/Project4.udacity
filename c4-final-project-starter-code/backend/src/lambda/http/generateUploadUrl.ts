137a5469336d83a8eb082bef80cb91cab106e14c
Quoc0711 <Quoctc11@gmail.com>
2022-12-01 13:49:22 +0700

    updated code

--- c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -4,22 +4,24 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 import { getUserId } from '../utils'
-import { AttachmentUtils } from '../../businessLogic/attachmentUtils'
+import { AttachmentUtils } from '../../dataLayer/attachmentUtils'
 
+const attachmentUtils = new AttachmentUtils();
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    const attachmentUtils = new AttachmentUtils();
+    console.log("event");
+    console.log(event);
+    const todoId = event.pathParameters.todoId ? event.pathParameters.todoId : ''
+
     const uploadUrl = await attachmentUtils.createAttachmentPresignedUrl(todoId, getUserId(event))
 
     return {
-      statusCode: 200,
+      statusCode: 201,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
-        todoId: todoId,
         uploadUrl: uploadUrl
       })
     }
